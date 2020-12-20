d8bcb1346c8aabcb08e082876f236e3dc1f3686a
Can Sakalöz <cansakaloz@gmail.com>
2020-12-20 15:59:53 +0300

    Add Connection String
    Use `UseNpgsql` method from Npgsql EntityFrameworkCore library
    to add the connection string for PostgreSQL database.

--- 20201220-aspnetcore-giris/ApplicationDbContext.cs
+++ 20201220-aspnetcore-giris/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ namespace _20201220_aspnetcore_giris
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Username=ozcan;Password=okasal01;Database=asptutorial");
     }
 }
