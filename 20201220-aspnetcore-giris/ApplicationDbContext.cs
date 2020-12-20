1cf8a060704a6e4a0b13ec403606610897404eff
Can Sakalöz <cansakaloz@gmail.com>
2020-12-20 16:04:15 +0300

    Add Category Model Class
    Category class is the model class for categories table.
    We add a base template for the class.

--- 20201220-aspnetcore-giris/ApplicationDbContext.cs
+++ 20201220-aspnetcore-giris/ApplicationDbContext.cs
@@ -7,4 +7,10 @@ namespace _20201220_aspnetcore_giris
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Host=localhost;Username=ozcan;Password=okasal01;Database=asptutorial");
     }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
 }
