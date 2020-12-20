286afb512bcaeadcfbf7f5d3998b9c2e86d4cc37
Can Sakalöz <cansakaloz@gmail.com>
2020-12-20 17:04:52 +0300

    Update Main Method for Sample CRUD
    Main method updated to insert two categories
    and one post for each category. We save the changes
    (commit) and then query the posts together with
    the corresponding category item and print to
    the console.

--- 20201220-aspnetcore-giris/Program.cs
+++ 20201220-aspnetcore-giris/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace _20201220_aspnetcore_giris
 {
@@ -6,7 +9,33 @@ namespace _20201220_aspnetcore_giris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var db = new ApplicationDbContext();
+
+            // var linuxCategory = new Category {Name = "Linux"};
+            // var windowsCategory = new Category {Name = "Windows"};
+            //
+            // db.Categories.Add(linuxCategory);
+            // db.Categories.Add(windowsCategory);
+            //
+            // var linuxPost = new Post {Title = "Linux is hot", Content = "I love it", Category = linuxCategory};
+            // var windowsPost = new Post
+            //     {Title = "Windows is great", Content = "Very nice.", Category = windowsCategory};
+            //
+            // db.Posts.Add(linuxPost);
+            // db.Posts.Add(windowsPost);
+            //
+            // db.SaveChanges();
+
+            var postList = db.Posts.ToList();
+            // postList.ForEach(x => Console.WriteLine($"Title: {x.Title}, Content: {x.Content}"));
+            foreach (var post in postList)
+            {
+                var category = db.Categories.Find(post.CategoryId);
+                Console.WriteLine($"Category: {category.Name}, Title: {post.Title}, Content: {post.Content}");
+            }
         }
     }
 }
+
+        
+        
