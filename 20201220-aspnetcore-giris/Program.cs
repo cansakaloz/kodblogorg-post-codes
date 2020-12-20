0c144789141dd98aefc170f98864dd317525a203
Can Sakalöz <cansakaloz@gmail.com>
2020-12-20 15:18:32 +0300

    Generate dotnet project
    Used `dotnet new console -o 20201220-aspnetcore-giris` command
    to generate an initial project for the blog post

---
+++ 20201220-aspnetcore-giris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20201220_aspnetcore_giris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
