d8c3c911431ae5cddadeff19719c8b7042d76c44
Can Sakalöz <cansakaloz@gmail.com>
2020-12-20 16:13:56 +0300

    Add Category Foreign Key to Post Class

--- 20201220-aspnetcore-giris/ApplicationDbContext.cs
+++ 20201220-aspnetcore-giris/ApplicationDbContext.cs
@@ -15,11 +15,16 @@ namespace _20201220_aspnetcore_giris
         public string Name { get; set; }
 
         public List<Post> Posts { get; set; }
+    }
 
     public class Post
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        
     }
 }
